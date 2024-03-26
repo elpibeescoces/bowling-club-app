0ed8d432a3fa34c250b71cfecdf25929842ce130
elliot <elpibeescoces@gmail.com>
2024-03-26 13:57:29 +0000

    all

---
+++ bowling_club_app/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from bowling_club_app.users.models import User
+from bowling_club_app.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture()
+def user(db) -> User:
+    return UserFactory()
